298b8efdefa9fbd8703d32d035bfa6ee96749244
ptuananhqt <ptuananhqt@gmail.com>
2021-03-16 20:16:27 +0700

    Thich ghi gi do ghi

---
+++ login.php
@@ -0,0 +1,22 @@
+<div class="_2dJqN" data-reactid="125">
+    <form data-reactid="126">
+        <h1 class="_2ua28" data-reactid="127">
+            <span data-reactid="128">Đăng nhập</span>
+        </h1><div class="OPeTF" data-reactid="129">
+            <div class="_2gedt" data-reactid="130">
+                <div data-reactid="131">
+                    <input type="text" name="username" value="" placeholder="Tên đăng nhập" class="_2K5yV _34Elg" data-reactid="132">
+                    <span class="_2SZYG">Vui lòng nhập tài khoản</span>
+                </div></div></div>
+        <div class="OPeTF" data-reactid="133">
+            <div class="_2gedt" data-reactid="134">
+                <div data-reactid="135">
+                    <input type="password" name="password" value="" placeholder="Mật khẩu" class="_2K5yV" data-reactid="136"></div>
+            </div></div>
+        <div class="_2IUTB" data-reactid="137">
+            <button type="submit" disabled="" class="_3JtEO _3bE6i" data-reactid="138">
+                <span data-reactid="139">Đăng nhập</span><!-- react-empty: 140 -->
+            </button>
+        </div>
+    </form>
+</div>
